7c50741be3ca5112c1c5105fe437ef6e1981036b
mayankkumar9871 <mayank0551604@gmail.com>
2026-04-08 15:29:00 +0530

    Update token_manager.py

--- app/token_manager.py
+++ app/token_manager.py
@@ -10,7 +10,7 @@ from datetime import timedelta
 
 logger = logging.getLogger(__name__)
 
-AUTH_URL = os.getenv("AUTH_URL", "http://jwt.thug4ff.com/token") 
+AUTH_URL = os.getenv("AUTH_URL", "http://jwt.thug4ff.xyz/token") 
 CACHE_DURATION = timedelta(hours=7).seconds
 TOKEN_REFRESH_THRESHOLD = timedelta(hours=6).seconds
 
