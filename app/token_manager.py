90146041582e2e93d5851519a9b86f053e150569
mayankkumar9871 <mayank0551604@gmail.com>
2026-04-08 15:58:32 +0530

    Update token_manager.py

--- app/token_manager.py
+++ app/token_manager.py
@@ -10,7 +10,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 logger = logging.getLogger(__name__)
 
-AUTH_URL = os.getenv("AUTH_URL", "http://jwt.thug4ff.xyz/token")
+AUTH_URL = os.getenv("AUTH_URL", "http://jwt.thug4ff.com/token")
 
 CACHE_DURATION = timedelta(hours=7).seconds
 TOKEN_REFRESH_THRESHOLD = timedelta(hours=6).seconds
